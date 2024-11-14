a921c1dc9083438b818368c455a3eb970abb9bb4
rohitpv88 <166278360+rohitpv88@users.noreply.github.com>
2024-11-15 00:53:59 +0530

    Add files via upload

---
+++ first_script.py
@@ -0,0 +1,41 @@
+import cv2
+import pickle
+
+width, height = 107, 48
+
+try:
+    with open('static/CarParkPos', 'rb') as f:
+        posList = pickle.load(f)
+except:
+    posList = []
+
+def mouseClick(events, x, y, flags, params):
+    if events == cv2.EVENT_LBUTTONDOWN:
+        posList.append((x, y))
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i, pos in enumerate(posList):
+            x1, y1 = pos
+            if x1 < x < x1 + width and y1 < y < y1 + height:
+                posList.pop(i)
+
+    with open('CarParkPos', 'wb') as f:
+        pickle.dump(posList, f)
+
+while True:
+    img = cv2.imread('static/carParkImg.png')
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
+
+    cv2.imshow("Image", img)
+    cv2.setMouseCallback("Image", mouseClick)
+
+    # Wait for a key press for 1ms
+    key = cv2.waitKey(1)
+
+    # Check if the 'Esc' key (ASCII 27) is pressed to break the loop and close the window
+    if key == 27:  # 27 is the ASCII value of the Escape key
+        print("Exiting the program...")
+        break
+
+# Close all OpenCV windows
+cv2.destroyAllWindows()
